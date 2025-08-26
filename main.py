e30b43844a611b5b901f8e531f749ce881ed59f9
Luisa <luisa.collodi@gmail.com>
2025-08-26 12:03:52 +0200

    Commit 6

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print('Hello World')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('Hi Dave3')
+    print('Hi Dave5')
     print('Bye Sam')
 
 
